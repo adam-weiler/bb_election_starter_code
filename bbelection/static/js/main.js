f49be10a6e490937973e714ae5f4581934705f71
adam-weiler <adam.weiler@yahoo.ca>
2019-08-23 14:21:35 -0400

    Creating JSON file just for backup in case API goes down again. Creating a form that allows user to vote for each candidate.

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,57 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // const url = "https://bb-election-api.herokuapp.com/";  // This endpoint is not working.
-    const url = 'https://eric-deploy-test.herokuapp.com/bb';  // Eric's API endpoint.
+    const url = "https://bb-election-api.herokuapp.com/";  // This endpoint is now working.
+    // const url = 'https://eric-deploy-test.herokuapp.com/bb';  // Eric's API endpoint.
     const request = axios.get(url);
     const allCandidatesList = document.querySelector('#all_candidates');
 
     request.then((response) => {
-        // console.log('-- Received response.');
+        console.log('-- Received response.');
 
         ourCandidates = response.data
-        // console.log(ourCandidates);
-
-        // ourResponse = {
-        //   "candidates": [
-        //     {
-        //       "id": "577805c3e30089e66c1ede16",
-        //       "name": "Spongebob",
-        //       "votes": 2
-        //     },
-        //     {
-        //       "id": "577805c3e30089e66c1ede18",
-        //       "name": "Squidward",
-        //       "votes": 1
-        //     },
-        //     {
-        //       "id": "577805c3e30089e66c1ede19",
-        //       "name": "Sandy",
-        //       "votes": 0
-        //     },
-        //     {
-        //       "id": "577805c3e30089e66c1ede17",
-        //       "name": "Patrick",
-        //       "votes": 0
-        //     },
-        //     {
-        //       "id": "577805c3e30089e66c1ede1a",
-        //       "name": "Gary",
-        //       "votes": 1
-        //     }
-        //   ]
-        // }
+        console.log(ourCandidates);
 
         ourCandidates['candidates'].forEach((element) => {
-            // console.log(element);
+            console.log(element);
 
             let dataElem = document.createElement('li');
-            dataElem.innerHTML = `${element.name} got ${element.votes} votes.`;
+            dataElem.innerHTML = `${element.name} got ${element.votes} votes - ${element.id}.
+            <form method="POST" action="https://bb-election-api.herokuapp.com/vote?id=">
+                <input type="hidden" name="id" value="${element.id}">    
+                <input type="submit" />
+            </form>`;
 
             allCandidatesList.append(dataElem);
         });
     })
     .catch((error) => {
-        // console.log('-- Received error.');
-        // console.log(error);
+        console.log('-- Received error.');
+        console.log(error);
 
         let dataElem = document.createElement('li');
         dataElem.innerHTML = `${error}<br/>So sorry for the inconvenience!`;
@@ -59,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function() {
         allCandidatesList.appendChild(dataElem);
     })
     .then(() => {
-        // console.log('-- Request is over regardless if it worked or not.');
-        // console.log('Hey, the request finished!');
+        console.log('-- Request is over regardless if it worked or not.');
+        console.log('Hey, the request finished!');
     })
 });
